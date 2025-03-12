ce4bb1c0d8e3dc2f983c2c2cc30a8fd91e181fea
fadatomisin <fadahunsitomisin2017@gmail.com>
2025-03-12 11:38:50 +0100

    Initial Commit

---
+++ src/boost_physiobookingsystem/Boost_PhysioBookingSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boost_physiobookingsystem;
+
+/**
+ *
+ * @author Office
+ */
+public class Boost_PhysioBookingSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
